5a6321a6a4a08401aab1266403c511c665127190
a20j30 <42009944+AJ4241@users.noreply.github.com>
2021-10-07 20:36:14 -0400

    updates to myPush and myIncludes

--- HW2webdev.js
+++ HW2webdev.js
@@ -5,3 +5,45 @@ Array.prototype.myEach = function() {
 
 Array.prototype.myMap = function() {
 };
+
+Array.prototype.myFilter = function() {
+};
+
+Array.prototype.mySome = function() {
+};
+
+Array.prototype.myEvery = function() {
+};
+
+Array.prototype.myReduce = function() {
+};
+
+Array.prototype.myIncludes = function(elements, checkElement) {
+  for (let x= 0; x < elements.length; x++) {
+    if (elements[x] == checkElement) {
+      return true;
+    }
+  }
+  return false;
+};
+
+Array.prototype.myIndexOf = function() {
+};
+
+Array.prototype.myPush = function(elements, elementToAdd) {
+  let newArray = [elementToAdd];
+  let modifiedElements = elements.concat(newArray);
+  return modifiedElements;
+};
+
+Array.prototype.myLastIndexOf = function() {
+};
+
+Object.grabKeys = function() {
+};
+
+Object.grabValues = function() {
+};
+
+
+
